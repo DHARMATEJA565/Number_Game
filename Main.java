818ae03e65b90bff3dc54bce415ab9e949efbe0f
DHARMATEJA565 <132571298+DHARMATEJA565@users.noreply.github.com>
2023-05-04 17:20:53 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,209 @@
+import javax.swing.*;
+import javax.swing.plaf.ButtonUI;
+import java.awt.*;
+import java.awt.event.*;
+
+class Main extends JFrame
+{
+    JTextField guess, bestscore, totalguess;
+    ButtonListener buttonListener;
+    ButtonListener2 buttonListener2;
+    ButtonListener3 buttonListener3;
+    ButtonListener4 buttonListener4;
+    JLabel inputlabel,guesslabel,trylabel,bestscorelabel,totalguesslabel,imglabel,titlelabel;
+
+    int rand=(int) (Math.random()*100);
+    int count=0;
+    public Main()
+    {
+        Container c = getContentPane();
+        c.setLayout(null);
+        c.setBackground(Color.WHITE);
+
+	    titlelabel = new JLabel(" ROUND 1 ");
+	    titlelabel.setForeground(Color.RED);
+        titlelabel.setFont(new Font("SERIF",Font.BOLD,28));
+        titlelabel.setSize(270,40);
+        titlelabel.setLocation(170,10);
+
+        guesslabel = new JLabel("Guess the Number ?");
+        guesslabel.setForeground(Color.BLUE);
+        guesslabel.setFont(new Font("SANSSERIF",Font.PLAIN,24));
+        guesslabel.setSize(270,20);
+        guesslabel.setLocation(120,90);
+
+        inputlabel=new JLabel("Enter a number between 1-100");
+        inputlabel.setFont(new Font("MONOSPACE",Font.PLAIN,14));
+        inputlabel.setSize(270,20);
+        inputlabel.setLocation(130,130);
+
+        trylabel = new JLabel("Try and guess it !");
+        trylabel.setFont(new Font("DIALOG",Font.ITALIC,20));
+	    trylabel.setForeground(Color.RED);
+        trylabel.setSize(290,40);
+        trylabel.setLocation(150,190);
+
+        guess = new JTextField(10);
+        guess.setSize(60,20);
+        guess.setLocation(190,160);
+   
+        
+        bestscore = new JTextField(10);
+        bestscore.setSize(30,20);
+        bestscore.setLocation(340,50);
+
+        bestscorelabel = new JLabel("Best Score");
+        bestscorelabel.setFont(new Font("monospace",Font.PLAIN,14));
+        bestscorelabel.setSize(270,20);
+        bestscorelabel.setLocation(380,50);
+
+        totalguess = new JTextField(10);
+        totalguess.setSize(30,20);
+        totalguess.setLocation(10,50);
+
+        totalguesslabel = new JLabel("Total Guesses");
+        totalguesslabel .setFont(new Font("dialog input",Font.PLAIN,14));
+        totalguesslabel.setSize(270,20);
+        totalguesslabel.setLocation(50,50);
+
+      
+        JButton guessbutton =new JButton("Guess");
+        guessbutton.setSize(100,30);
+        guessbutton.setLocation(170,240);
+        guessbutton.setBackground(Color.LIGHT_GRAY);
+        buttonListener = new ButtonListener();
+        guessbutton.addActionListener(buttonListener);
+
+        JButton giveupbutton = new JButton("Give up!");
+        giveupbutton.setSize(100,30);
+        giveupbutton.setLocation(110,280);
+        giveupbutton.setBackground(Color.LIGHT_GRAY);
+        buttonListener2 = new ButtonListener2();
+        giveupbutton.addActionListener(buttonListener2);
+
+        JButton playagainbutton = new JButton("Play Again!");
+        playagainbutton.setSize(100,30);
+        playagainbutton.setLocation(230,280);
+        playagainbutton.setBackground(Color.LIGHT_GRAY);
+        buttonListener3 = new ButtonListener3();
+        playagainbutton.addActionListener(buttonListener3);
+
+        JButton round2 = new JButton("ROUND 2");
+        round2.setSize(100,30);
+        round2.setLocation(170,330);
+        round2.setBackground(Color.LIGHT_GRAY);
+        buttonListener4 = new ButtonListener4();
+        round2.addActionListener(buttonListener4);
+        
+        c.add(bestscorelabel);
+        c.add(totalguesslabel);
+        c.add(trylabel);
+        c.add(guesslabel);
+        c.add(inputlabel);
+      
+        c.add(guess);
+        c.add(bestscore);
+        c.add(totalguess);
+        c.add(guessbutton);
+        c.add(giveupbutton);
+        c.add(playagainbutton);
+	    c.add(titlelabel);
+	    c.add(round2);
+        bestscore.setEditable(false);
+        totalguess.setEditable(false);
+        
+        setTitle("GUESS THE NUMBER");
+        setSize(500,400);
+        setLocation(300,200);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private class ButtonListener implements ActionListener
+    {
+        int bestScore=150;
+        public void actionPerformed(ActionEvent e)
+        {   
+            int a = Integer.parseInt(guess.getText());
+            count=count+1;
+
+            if (count ==15)
+            {
+            	trylabel.setText("You lose the game play again !");
+            	count =0;
+             }
+
+            else {
+            	if(a<rand)
+            	{
+            		trylabel.setText(a+" is low, try again!!");
+            		}
+            	else if(a>rand)
+            	{
+            		trylabel.setText(a+" is high, try again!!");
+            		}
+            	else
+            	{
+            		trylabel.setText("Your guess is correct, You win!!");
+            		if(count==1)
+            		{
+            			bestScore=150;
+            			bestscore.setText(bestScore+"");
+            			}
+            		else if (count*10<bestScore) {
+            			bestScore = 150 - count*10;
+            			bestscore.setText(bestScore+"");
+            			}
+            		else {
+            			
+            			bestscore.setText("" + bestScore);
+            			}
+            		}
+            	guess.setEditable(true);
+            	}
+            guess.requestFocus();
+            guess.selectAll();
+            totalguess.setText(count+"");
+            }
+        }
+
+    private class ButtonListener2 implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            totalguess.setText("");
+            trylabel.setText("The correct answer is "+rand+"!!");
+            guess.setText("");
+            guess.setEditable(false);
+        }
+    }
+
+    private class ButtonListener3 implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            rand=(int) (Math.random()*100);
+            guess.setText("");
+            totalguess.setText("");
+            trylabel.setText("Try and guess it !");
+            totalguess.setText("");
+            count=0;
+            guess.setEditable(true);
+            guess.requestFocus();
+        }
+    }
+    
+    
+    private class ButtonListener4 implements ActionListener
+    {
+    	public void actionPerformed(ActionEvent e)
+    	{
+    		Main1 m1 = new Main1();
+    		m1.setVisible(true);
+    	}
+    }
+    public static void main(String[] args)
+    {
+        new Main();
+    }
+}
---
+++ Main1.java
@@ -0,0 +1,208 @@
+
+import javax.swing.*;
+import javax.swing.plaf.ButtonUI;
+import java.awt.*;
+import java.awt.event.*;
+
+class Main1 extends JFrame
+{
+    JTextField guess, bestscore, totalguess;
+    ButtonListener buttonListener;
+    ButtonListener2 buttonListener2;
+    ButtonListener3 buttonListener3;
+    ButtonListener4 buttonListener4;
+    JLabel inputlabel,guesslabel,trylabel,bestscorelabel,totalguesslabel,imglabel,titlelabel;
+
+    int rand=(int) (Math.random()*100);
+    int count=0;
+    public Main1()
+    {
+        Container c = getContentPane();
+        c.setLayout(null);
+        c.setBackground(Color.WHITE);
+
+	    titlelabel = new JLabel(" ROUND 2 ");
+	    titlelabel.setForeground(Color.BLUE);
+        titlelabel.setFont(new Font("times new roman",Font.BOLD,28));
+        titlelabel.setSize(270,40);
+        titlelabel.setLocation(170,10);
+
+        guesslabel = new JLabel("Guess the Number ?");
+        guesslabel.setForeground(Color.RED);
+        guesslabel.setFont(new Font("monospace",Font.ITALIC,24));
+        guesslabel.setSize(270,20);
+        guesslabel.setLocation(120,90);
+
+        inputlabel=new JLabel("Enter a number between 1-100");
+        inputlabel.setFont(new Font("MONOSPACE",Font.PLAIN,14));
+        inputlabel.setSize(270,20);
+        inputlabel.setLocation(130,130);
+
+        trylabel = new JLabel("Try and guess it !");
+        trylabel.setFont(new Font("DIALOG",Font.ITALIC,20));
+	    trylabel.setForeground(Color.BLUE);
+        trylabel.setSize(290,40);
+        trylabel.setLocation(150,190);
+
+        guess = new JTextField(10);
+        guess.setSize(60,20);
+        guess.setLocation(190,160);
+
+        bestscore = new JTextField(10);
+        bestscore.setSize(30,20);
+        bestscore.setLocation(340,50);
+
+        bestscorelabel = new JLabel("Best Score");
+        bestscorelabel.setFont(new Font("monospace",Font.PLAIN,14));
+        bestscorelabel.setSize(270,20);
+        bestscorelabel.setLocation(380,50);
+
+        totalguess = new JTextField(10);
+        totalguess.setSize(30,20);
+        totalguess.setLocation(10,50);
+
+        totalguesslabel = new JLabel("Total Guesses");
+        totalguesslabel .setFont(new Font("dialog input",Font.PLAIN,14));
+        totalguesslabel.setSize(270,20);
+        totalguesslabel.setLocation(50,50);
+
+      
+        JButton guessbutton =new JButton("Guess");
+        guessbutton.setSize(100,30);
+        guessbutton.setLocation(170,240);
+        guessbutton.setBackground(Color.LIGHT_GRAY);
+        buttonListener = new ButtonListener();
+        guessbutton.addActionListener(buttonListener);
+
+        JButton giveupbutton = new JButton("Give up!");
+        giveupbutton.setSize(100,30);
+        giveupbutton.setLocation(110,280);
+        giveupbutton.setBackground(Color.LIGHT_GRAY);
+        buttonListener2 = new ButtonListener2();
+        giveupbutton.addActionListener(buttonListener2);
+
+        JButton playagainbutton = new JButton("Play Again!");
+        playagainbutton.setSize(100,30);
+        playagainbutton.setLocation(230,280);
+        playagainbutton.setBackground(Color.LIGHT_GRAY);
+        buttonListener3 = new ButtonListener3();
+        playagainbutton.addActionListener(buttonListener3);
+
+        JButton round3 = new JButton("ROUND 3");
+        round3.setSize(100,30);
+        round3.setLocation(170,330);
+        round3.setBackground(Color.LIGHT_GRAY);
+        buttonListener4 = new ButtonListener4();
+        round3.addActionListener(buttonListener4);
+        
+        c.add(bestscorelabel);
+        c.add(totalguesslabel);
+        c.add(trylabel);
+        c.add(guesslabel);
+        c.add(inputlabel);
+        c.add(guess);
+        c.add(bestscore);
+        c.add(totalguess);
+        c.add(guessbutton);
+        c.add(giveupbutton);
+        c.add(playagainbutton);
+	    c.add(titlelabel);
+	    c.add(round3);
+        bestscore.setEditable(false);
+        totalguess.setEditable(false);
+        
+        setTitle("GUESS THE NUMBER");
+        setSize(500,400);
+        setLocation(300,200);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private class ButtonListener implements ActionListener
+    {
+        int bestScore=100;
+        public void actionPerformed(ActionEvent e)
+        {   
+            int a = Integer.parseInt(guess.getText());
+            count=count+1;
+
+            if (count ==10)
+            {
+            	trylabel.setText("You lose the game play again !");
+            	count =0;
+             }
+
+            else {
+            	if(a<rand)
+            	{
+            		trylabel.setText(a+" is low, try again!!");
+            		}
+            	else if(a>rand)
+            	{
+            		trylabel.setText(a+" is high, try again!!");
+            		}
+            	else
+            	{
+            		trylabel.setText("Your guess is correct, You win!!");
+            		if(count==1)
+            		{
+            			bestScore=100;
+            			bestscore.setText(bestScore+"");
+            			}
+            		else if (count*10<bestScore) {
+            			bestScore = 100 - count*10;
+            			bestscore.setText(bestScore+"");
+            			}
+            		else {
+            			
+            			bestscore.setText("" + bestScore);
+            			}
+            		}
+            	guess.setEditable(true);
+            	}
+            guess.requestFocus();
+            guess.selectAll();
+            totalguess.setText(count+"");
+            }
+        }
+
+    private class ButtonListener2 implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            totalguess.setText("");
+            trylabel.setText("The correct answer is "+rand+"!!");
+            guess.setText("");
+            guess.setEditable(false);
+        }
+    }
+
+    private class ButtonListener3 implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            rand=(int) (Math.random()*100);
+            guess.setText("");
+            totalguess.setText("");
+            trylabel.setText("Try and guess it !");
+            totalguess.setText("");
+            count=0;
+            guess.setEditable(true);
+            guess.requestFocus();
+        }
+    }
+    
+    
+    private class ButtonListener4 implements ActionListener
+    {
+    	public void actionPerformed(ActionEvent e)
+    	{
+    		Main2 m2 = new Main2();
+    		m2.setVisible(true);
+    	}
+    }
+    public static void main(String[] args)
+    {
+        new Main1();
+    }
+}
